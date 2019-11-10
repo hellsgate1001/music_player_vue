147e37d2245b41257d47a5694762b3aee2622d45
Brian Wilson <brianwilson1001@hotmail.com>
2019-11-10 18:24:51 +0000

    Add REST calls for data population

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        The Wilson Jukebox
+      </v-card-title>
+    </v-card>
+
+    <v-container fluid=true fill-height>
+      <v-row>
+        <v-col cols="12" md="4" id="artists" style="overflow:auto">
+          <v-list>
+            <v-subheader>
+              Artists
+            </v-subheader>
+            <v-list-item-group id="artist-group">
+              <v-list-item v-for="artist in artists" :key="artist" v-on:click="getAlbums(artist)">
+                {{ artist }}
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+
+        <v-col cols="12" md="4" id="albums">
+          <v-list>
+            <v-subheader>
+              Albums
+            </v-subheader>
+            <v-list-item-group>
+              <v-list-item v-for="album in albums" :key="album" v-on:click="getSongs(album)">
+                {{ album }}
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+
+        <v-col cols="12" md="4" id="songs">
+          <v-list>
+            <v-subheader>
+              Songs
+            </v-subheader>
+            <v-list-item-group>
+              <v-list-item v-for="song in songs" :key="song[0]">
+                {{ song[0] }}: {{ song[1] }}
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint no-console: 0 */
+import axios from 'axios';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => {
+    return {
+      artists: [],
+      artist: '',
+      albums: [],
+      songs: []
+    }
+  },
+  created() {
+    axios.get('http://192.168.1.20:5000/artists')
+      .then((response) => {
+        console.log('Setting albums');
+        this.artists = response.data;
+      });
+  },
+  methods: {
+    getAlbums(artist) {
+      this.artist = artist;
+      axios.get('http://192.168.1.20:5000/artist/' + artist)
+        .then((response) => {
+        console.log('ARTISTS 1');
+        console.log(this.artists);
+          this.albums = response.data;
+        });
+    },
+    albumsCall() {
+    },
+    getSongs(album) {
+      axios.get('http://192.168.1.20:5000/artist/' + this.artist + '/' + album)
+        .then((response) => {
+          this.songs = response.data;
+        });
+    }
+  }
+};
+</script>
