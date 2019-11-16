fe4561be58a0a3c991977ee701003ec87bb8f472
Brian Wilson <brianwilson1001@hotmail.com>
2019-11-16 03:04:33 +0000

    Add playlist component

--- src/App.vue
+++ src/App.vue
@@ -6,91 +6,182 @@
       </v-card-title>
     </v-card>
 
-    <v-container fluid=true fill-height>
-      <v-row>
-        <v-col cols="12" md="4" id="artists" style="overflow:auto">
-          <v-list>
-            <v-subheader>
-              Artists
-            </v-subheader>
-            <v-list-item-group id="artist-group">
-              <v-list-item v-for="artist in artists" :key="artist" v-on:click="getAlbums(artist)">
-                {{ artist }}
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-col>
+    <v-container style="height: 85vh">
+      <v-stepper v-model="e1" style="height: 100%; overflow:auto;">
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <v-list>
+              <v-subheader>
+                <span class="title">Artists</span>
+              </v-subheader>
+              <v-list-item-group id="artist-group">
+                <v-list-item
+                  v-for="artist in artists"
+                  :key="artist"
+                  v-on:click="getAlbums(artist)"
+                >
+                  {{ artist }}
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-stepper-content>
+          <v-stepper-content step="2">
+            <v-list>
+              <v-subheader>
+                <span class="title">Albums</span>
+                <span style="margin-left: 1em; cursor: pointer;" @click="e1 = 1"
+                  >Back to artists</span
+                >
+              </v-subheader>
+              <v-list-item-group>
+                <v-list-item
+                  v-for="album in albums"
+                  :key="album"
+                  v-on:click="getSongs(album)"
+                >
+                  {{ album }}
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-stepper-content>
+          <v-stepper-content step="3">
+            <v-list>
+              <v-subheader>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-icon style="margin-right: 1em;" v-on="on" @click="e1 = 2">mdi-arrow-left</v-icon>
+                  </template>
+                  <span>Back to albums</span>
+                </v-tooltip>
+                <span class="title">Songs</span>
+                <v-menu offset-y>
+                  <template v-slot:activator="{ on }">
+                    <v-icon style="margin-left: 1em; position: absolute; right: 1em;" v-on="on">mdi-menu</v-icon>
+                  </template>
 
-        <v-col cols="12" md="4" id="albums">
-          <v-list>
-            <v-subheader>
-              Albums
-            </v-subheader>
-            <v-list-item-group>
-              <v-list-item v-for="album in albums" :key="album" v-on:click="getSongs(album)">
-                {{ album }}
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-col>
-
-        <v-col cols="12" md="4" id="songs">
-          <v-list>
-            <v-subheader>
-              Songs
-            </v-subheader>
-            <v-list-item-group>
-              <v-list-item v-for="song in songs" :key="song[0]">
-                {{ song[0] }}: {{ song[1] }}
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-col>
-      </v-row>
-
+                  <v-list>
+                    <v-list-item @click="playAll()">
+                      <v-list-item-title>Play All</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item @click="addAll()">
+                      <v-list-item-title>Add All To Playlist</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-subheader>
+              <v-list-item-group>
+                <v-list-item v-for="(song, i) in songs" :key="song[0]">
+                  <v-hover>
+                    <v-icon slot-scope="{ hover }" :class="`${ hover ? 'green--text': '' }`">
+                      mdi-play
+                    </v-icon>
+                  </v-hover>
+                  <v-icon
+                    style="margin-right: 0.3em;"
+                    :class="{ 'd-none': !plus[i] }"
+                    :data-tracknum="song[0]"
+                    @click="addToPlaylist(i, $event)"
+                  >
+                    mdi-plus
+                  </v-icon>
+                  <v-icon
+                    style="margin-right: 0.3em;"
+                    :class="{ 'd-none': plus[i] }"
+                    :data-tracknum="song[0]"
+                    @click="removeFromPlaylist(i)"
+                  >
+                    mdi-minus
+                  </v-icon>
+                  {{ song[0] }}: {{ song[1] }}
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
     </v-container>
+
+    <playlist></playlist>
   </v-app>
 </template>
 
 <script>
 /* eslint no-console: 0 */
 import axios from 'axios';
+import Playlist from './playlist/playlist.component.vue';
+
 
 export default {
   name: 'App',
+  components: {
+    Playlist
+  },
 
   data: () => {
     return {
       artists: [],
       artist: '',
       albums: [],
-      songs: []
-    }
+      album: '',
+      songs: [],
+      e1: 0,
+      plus: []
+    };
   },
   created() {
-    axios.get('http://192.168.1.20:5000/artists')
-      .then((response) => {
-        console.log('Setting albums');
-        this.artists = response.data;
-      });
+    axios.get('http://192.168.1.20:5000/artists').then((response) => {
+      this.artists = response.data;
+    });
   },
   methods: {
+    addAll() {
+      axios.get(`http://192.168.1.20:5000/add-album-to-playlist/${this.artist}/${this.album}`).then(() => {
+        console.log('Added');
+      });
+    },
+    addToPlaylist(i, event) {
+      const tracknum = event.target.getAttribute('data-tracknum');
+      axios
+        .get(
+          `http://192.168.1.20:5000/add-to-playlist/${this.artist}/${this.album}/${tracknum}`
+        )
+        .then(() => {
+          this.plus[i] = false;
+          this.$forceUpdate();
+        });
+    },
     getAlbums(artist) {
       this.artist = artist;
-      axios.get('http://192.168.1.20:5000/artist/' + artist)
-        .then((response) => {
-        console.log('ARTISTS 1');
-        console.log(this.artists);
-          this.albums = response.data;
-        });
-    },
-    albumsCall() {
+      axios.get(`http://192.168.1.20:5000/artist/${artist}`).then(response => {
+        this.albums = response.data;
+        this.e1 = 2;
+      });
     },
     getSongs(album) {
-      axios.get('http://192.168.1.20:5000/artist/' + this.artist + '/' + album)
-        .then((response) => {
+      this.album = album;
+      axios
+        .get(`http://192.168.1.20:5000/artist/${this.artist}/${album}`)
+        .then(response => {
           this.songs = response.data;
+          this.e1 = 3;
+
+          this.plus = [];
+          this.minus = [];
+          const numSongs = this.songs.length;
+          for (let i = 0; i < numSongs; i++) {
+            this.plus[i] = true;
+          }
         });
+    },
+    playAll() {
+      console.log('Play All');
+    },
+    removeFromPlaylist(i) {
+      this.plus[i] = true;
+      this.$forceUpdate();
+      axios.get('http://192.168.1.20:5000/get-medialist').then(response => {
+        console.log(response);
+      });
     }
   }
 };
---
+++ src/playlist/playlist.component.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-list>
+        <v-subheader>
+            <span class="title">Playlist</span>
+        </v-subheader>
+        <v-list-item-group>
+            <v-list-item v-for="(track, i) in playlist" :key="i">
+                <v-hover>
+                    <v-icon slot-scope="{ hover }" :class="`${ hover ? 'green--text': '' }`">
+                        mdi-play
+                    </v-icon>
+                </v-hover>
+                {{ track[0] }} - {{ track[2] }}
+            </v-list-item>
+        </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+/* eslint no-console: 0 */
+import axios from 'axios';
+
+export default {
+    name: 'Playlist',
+
+    data: () => {
+        return {
+            playlist: []
+        }
+    },
+
+    created() {
+        axios.get('http://192.168.1.20:5000/get-playlist').then((response) => {
+            this.playlist = response.data;
+        });
+    }
+}
+</script>
